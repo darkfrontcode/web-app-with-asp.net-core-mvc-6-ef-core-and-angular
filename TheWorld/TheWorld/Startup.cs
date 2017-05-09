85d3ade1b06de1b73a52de151b32aaf6bb4afe9c
Ueslei de Souza <darkfrontcode@gmail.com>
2017-05-09 11:19:21 -0300

    side bar menu

--- TheWorld/TheWorld/Startup.cs
+++ TheWorld/TheWorld/Startup.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TheWorld
 {
@@ -13,6 +20,6 @@ namespace TheWorld
         {
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
-        }
+		}
     }
 }
---
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// site.js
+(function startup() {
+
+	var sidebar = document.getElementById("sidebar");
+	var wrapper = document.getElementById("wrapper");
+	var toggleList = new Array(sidebar, wrapper);
+
+	var sidebarToggle = document.getElementById("sideBarToggle");
+
+	sidebarToggle.onclick = function(){
+		toggleList.forEach(function(item) {
+            item.classList.toggle("hide-sidebar");
+            var hasSidebar = item.classList.contains("hide-sidebar");
+            sidebarToggle.innerHTML = hasSidebar ? "Show Sidebar" : "hide Sidebar";
+        });
+	}
+
+})();
