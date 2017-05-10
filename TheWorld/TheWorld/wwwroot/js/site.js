13be9fff6a48dfd633a23e2c401532f9f6deb389
Ueslei de Souza <darkfrontcode@gmail.com>
2017-05-10 18:06:54 -0300

    Adding some bootstrapping

--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -1,18 +1,24 @@
 ﻿// site.js
 (function startup() {
 
-	var sidebar = document.getElementById("sidebar");
-	var wrapper = document.getElementById("wrapper");
-	var toggleList = new Array(sidebar, wrapper);
+    var sidebar = document.getElementById("sidebar");
+    var wrapper = document.getElementById("wrapper");
+    var toggleList = new Array(sidebar, wrapper);
 
-	var sidebarToggle = document.getElementById("sideBarToggle");
+    var sidebarToggle = document.getElementById("sideBarToggle");
 
-	sidebarToggle.onclick = function(){
-		toggleList.forEach(function(item) {
+    sidebarToggle.onclick = function () {
+        toggleList.forEach(function (item) {
+
             item.classList.toggle("hide-sidebar");
+
             var hasSidebar = item.classList.contains("hide-sidebar");
-            sidebarToggle.innerHTML = hasSidebar ? "Show Sidebar" : "hide Sidebar";
+            var direction = hasSidebar ? "glyphicon-chevron-right" : "glyphicon-chevron-left";
+            var className = "glyphicon " + direction;
+
+            sidebarToggle.querySelector("i").setAttribute("class", className);
+
         });
-	}
+    }
 
 })();
